0e7131d38c87aa3606ff9fc42ad549757e4044f7
ahmedkaimkhani <ak9302359@gmail.com>
2024-04-29 02:18:19 +0500

    Learning Provider

--- lib/provider/favourite_provider.dart
+++ lib/provider/favourite_provider.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/material.dart';
 
-class FavouriteItemProvider with ChangeNotifier {}
+class FavouriteItemProvider with ChangeNotifier {
+  final List<int> _selectedItems = [];
+
+  List<int> get selectedItems => _selectedItems;
+
+  void addItems(value) {
+    selectedItems.add(value);
+  }
+}
